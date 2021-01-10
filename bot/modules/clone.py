8e8680f48f3f8403b9fde639e863ec93130d4500
Monk Tea <71178046+MonkTeam@users.noreply.github.com>
2021-01-10 13:06:33 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -5,20 +5,23 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.bot_utils import new_thread
 from bot import dispatcher
-
-
+ 
+ 
 @new_thread
 def cloneNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)
     if len(args) > 1:
         link = args[1]
-        msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
+        msg = sendMessage(f"𝗖𝗹𝗼𝗻𝗶𝗻𝗴..𝗪𝗮𝗶𝘁 𝗽𝗹𝘀.🤓\n\n 𝗟𝗶𝗻𝗸: <code>{link}</code>",context.bot,update)
         gd = GoogleDriveHelper()
-        result = gd.clone(link)
+        result, button = gd.clone(link)
         deleteMessage(context.bot,msg)
-        sendMessage(result,context.bot,update)
+        if button == "":
+            sendMessage(result,context.bot,update)
+        else:
+            sendMarkup(result,context.bot,update,button)
     else:
-        sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
-
+        sendMessage("𝗣𝗿𝗼𝘃𝗶𝗱𝗲 𝗚-𝗗𝗿𝗶𝘃𝗲 𝗦𝗵𝗮𝗿𝗲𝗮𝗯𝗹𝗲 𝗟𝗶𝗻𝗸 𝘁𝗼 𝗖𝗹𝗼𝗻𝗲😐.",context.bot,update)
+ 
 clone_handler = CommandHandler(BotCommands.CloneCommand,cloneNode,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
 dispatcher.add_handler(clone_handler)
