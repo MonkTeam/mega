131a0deace70a33e7b15b6ef54c8bad91f34cd75
Monk Tea <71178046+MonkTeam@users.noreply.github.com>
2021-01-10 14:41:37 +0530

    Update cancel_mirror.py

--- bot/modules/cancel_mirror.py
+++ bot/modules/cancel_mirror.py
@@ -1,15 +1,15 @@
 from telegram.ext import CommandHandler, run_async
-
+ 
 from bot import download_dict, dispatcher, download_dict_lock, DOWNLOAD_DIR
 from bot.helper.ext_utils.fs_utils import clean_download
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import *
-
+ 
 from time import sleep
 from bot.helper.ext_utils.bot_utils import getDownloadByGid, MirrorStatus
-
-
+ 
+ 
 @run_async
 def cancel_mirror(update, context):
     args = update.message.text.split(" ", maxsplit=1)
@@ -18,7 +18,7 @@ def cancel_mirror(update, context):
         gid = args[1]
         dl = getDownloadByGid(gid)
         if not dl:
-            sendMessage(f"GID: <code>{gid}</code> not found.", context.bot, update)
+            sendMessage(f"𝗚𝗜𝗗: <code>{gid}</code> not found.", context.bot, update)
             return
         with download_dict_lock:
             keys = list(download_dict.keys())
@@ -32,7 +32,7 @@ def cancel_mirror(update, context):
         if mirror_message is None or mirror_message.message_id not in keys:
             if BotCommands.MirrorCommand in mirror_message.text or \
                     BotCommands.TarMirrorCommand in mirror_message.text:
-                msg = "Mirror already have been cancelled"
+                msg = "𝗧𝗼𝗿𝗿𝗲𝗻𝘁 𝗮𝗹𝗿𝗲𝗮𝗱𝘆 𝗵𝗮𝘃𝗲 𝗯𝗲𝗲𝗻 𝗖𝗮𝗻𝗰𝗲𝗹𝗹𝗲𝗱"
                 sendMessage(msg, context.bot, update)
                 return
             else:
@@ -49,8 +49,8 @@ def cancel_mirror(update, context):
         dl.download().cancel_download()
     sleep(1)  # Wait a Second For Aria2 To free Resources.
     clean_download(f'{DOWNLOAD_DIR}{mirror_message.message_id}/')
-
-
+ 
+ 
 @run_async
 def cancel_all(update, context):
     with download_dict_lock:
@@ -62,8 +62,8 @@ def cancel_all(update, context):
                 count += 1
     delete_all_messages()
     sendMessage(f'Cancelled {count} downloads!', context.bot, update)
-
-
+ 
+ 
 cancel_mirror_handler = CommandHandler(BotCommands.CancelMirror, cancel_mirror,
                                        filters=(CustomFilters.authorized_chat | CustomFilters.authorized_user) & CustomFilters.mirror_owner_filter)
 cancel_all_handler = CommandHandler(BotCommands.CancelAllCommand, cancel_all,
